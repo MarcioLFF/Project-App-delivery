4b43ada2efd935fe954bc48033bdf4cdf92ca87d
tinho1123 <tinhoefaela@gmail.com>
2022-07-13 15:07:43 -0300

    :sparkles: feat: Create redirect to / for /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './pages/Login';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/login');
+  }, [navigate]);
+
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Routes>
+        <Route path="/login" element={ Login } />
+      </Routes>
+      <Header />
     </div>
   );
 }
