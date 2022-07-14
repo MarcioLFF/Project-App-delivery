30e2a2b863462d6719bf7150391d219ce2bd8f92
ladcs <hpladcs@gmail.com>
2022-07-14 14:46:38 -0300

    feat: seed users create

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('../routes');
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(cors());
 
 app.use('/api', routes);
 
